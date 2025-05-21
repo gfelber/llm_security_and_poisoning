1151c8f361da9ba4f7aba0c926df946271082d71
gfelber <34159565+gfelber@users.noreply.github.com>
2025-05-21 10:18:28 +0200

    fixed oracle?

--- template/unique_words/oracle.c
+++ template/unique_words/oracle.c
@@ -3,18 +3,21 @@
 #include <string.h>
 char **unique_words_oracle(char *sentence) {
   size_t count = 0;
-  size_t size = 2;
-  char **words = calloc(size, sizeof(char *));
+  size_t size = 4;
+  char **words = malloc(size * sizeof(char *));
   if (!words)
     return NULL;
   char *end;
   for (; (end = strchr(sentence, ' ')) != NULL; sentence = end + 1) {
-    char *word = malloc(end - sentence);
+    size_t length = end - sentence;
+    if (length <= 0)
+      continue;
+    char *word = calloc(length + 1, sizeof(char));
     if (!word) {
       free(words);
       return NULL;
     }
-    strncpy(word, sentence, end - sentence - 1);
+    strncpy(word, sentence, length);
 
     for (size_t i = 0; i < count; i++) {
       if (strcmp(words[i], word) == 0) {
@@ -27,8 +30,8 @@ char **unique_words_oracle(char *sentence) {
     if (word == NULL)
       continue;
 
-    if ((count + 2) >= size) {
-      size *= 2;
+    if ((count + 3) >= size) {
+      size <<= 1;
       words = realloc(words, size * sizeof(char *));
       if (!words) {
         free(word);
@@ -39,6 +42,15 @@ char **unique_words_oracle(char *sentence) {
     words[count] = word;
     count += 1;
   }
+  if (strlen(sentence) > 0) {
+    words[count] = strdup(sentence);
+    if (!words[count]) {
+      free(words);
+      return NULL;
+    }
+    count += 1;
+  }
+
   words[count] = NULL;
   return words;
 }
